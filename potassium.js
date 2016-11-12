da4be68fc6c659d42800911bb50ac52cc800c1bc
Trevor F. Smith <trevor@trevor.smith.name>
2016-11-12 08:43:49 -0800

    Rough out the major pieces.

---
+++ potassium.js
@@ -0,0 +1,480 @@
+"use strict"
+/*
+
+	Potassium is a reactive element framework.
+	It handles server backed data: k.DataModel and k.DataCollection
+	It handles UI logic: k.Component
+	It includes a DOM manipulation tool: k.el
+	It has a test suite in potassium-test.js
+	See http://PotassiumES.com/ for documentation and examples.
+
+*/
+
+var k = {}; // Potassium root object, named k because that is potassium's symbol on the periodic table. 
+
+/*
+	EventListener holds information about listeners on an object with the eventMixin
+*/
+k.EventListener = class {
+	constructor(eventName, callback){
+		this.eventName = eventName
+		this.callback = callback
+	}
+	matches(eventName){
+		return this.eventName === "all" || eventName === this.eventName
+	}
+	distributeEvent(eventName, ...params){
+		if(this.matches(eventName)){
+			this.callback(eventName, ...params)
+		}
+	}
+}
+
+/*
+	Mix in k.eventMixin to enable the instances to track event listeners and send them events
+	Use it like so: var YourClass = k.eventMixin(class { ... })
+	See k.DataObject for an example.
+*/
+k.eventMixin = Base => class extends Base {
+	trigger(eventName, ...params){
+		// Send an event to listeners
+		for(let listener of this.listeners){
+			listener.distributeEvent(eventName, ...params)
+		}
+	}
+	addListener(callback, eventName="all"){
+		this.listeners.push(new k.EventListener(eventName, callback))
+	}
+	removeListener(callback, eventName=null){
+		let remove = false
+		for(var i=0; i < this.listeners.length; i++){
+			remove = false
+			if(this.listeners[i].callback === callback){
+				if(eventName == null){
+					remove = true
+				} else if(this.listeners[i].matches(eventName)) {
+					remove = true
+				}
+			}
+			if(remove){
+				this.listeners.splice(i, 1)
+				i -= 1
+			}
+		}
+	}
+	clearListeners(){
+		this._listeners.length = 0
+	}
+	get listeners(){
+		// Returns an array of EventListener instances
+		if(typeof this._listeners == "undefined"){
+			this._listeners = []
+		}
+		return this._listeners
+	}
+	clearListeners(){
+		if(this._listeners){
+			this._listeners.length = 0
+		}
+	}
+}
+
+/*
+	The parent class for k.DataModel and k.DataCollection
+	It holds the event mixin and the generic function of fetching data from a remote service
+*/
+k.DataObject = k.eventMixin(class {
+	constructor(options={}){
+		this.options = options
+	}
+	cleanup(){
+		this.clearListeners()
+	}
+
+	// Return the URL (relative or full) as a string for the endpoint used by this.fetch
+	get url(){
+		throw new Error("Extending classes must implement url()")
+	}
+
+	// Clear out old data and set it to data
+	reset(data={}){
+		throw new Error("Extending classes must implement reset")
+	}
+	parse(data){
+		// Extending classes can override this to parse the data received via a fetch
+		return data
+	}
+	equals(obj){
+		// Extending classes can override this to allow less strict equality
+		return this === obj
+	}
+	fetch(){
+		// Ask the server for data for this model or collection
+		return new Promise(function(resolve, reject){
+			this.trigger("fetching", this)
+			fetch(this.url).then(response => response.json()).then(data => {
+				data = this.parse(data)
+				this.reset(data)
+				this.trigger("fetched", this, data, null)
+				resolve()
+			}).catch(err => {
+				this.trigger("fetched", this, null, err)
+				reject(err)
+			})
+		}.bind(this))
+	}
+})
+
+k._NO_CHANGE = Symbol()
+
+/*
+	DataModel holds a map of string,value pairs, sometimes fetched from or sent to a back-end server.
+	It fires events when values are changed.
+
+	options:
+		fieldDataObjects ({}): a map of fieldName (string) to k.DataObject (class), used to create sub-object in this Model's data
+*/
+k.DataModel = class extends k.DataObject {
+	constructor(data={}, options={}){
+		super(options)
+		if(typeof this.options.fieldDataObjects === "undefined"){
+			this.options.fieldDataObjects = {}
+		}
+		this.data = data || {}
+	}
+	cleanup() {
+		super.cleanup()
+		this.data = null
+	}
+	/* 
+		Find a value held within this k.DataModel. 
+		Return values may be native types or, if mapped by options.fieldDataObjects, another k.DataObject
+	*/
+	get(fieldName, defaultValue=null){
+		if(typeof this.data[fieldName] === "undefined" || this.data[fieldName] === null || this.data[fieldName] === ""){
+			return defaultValue
+		}
+		return this.data[fieldName]
+	}
+	// Set a key/value pair.
+	set(fieldName, value){
+		var batch = {}
+		batch[fieldName] = value
+		return this.setBatch(batch)
+	}
+	/*
+		Set a group of values. The 'values' parameter should be an object that works in for(key in values) loops like a dictionary: {}
+		If a key is in options.fieldDataObjects then the value will be used to contruct a k.DataObject and that will be the saved value.
+	*/
+	setBatch(values){
+		let changes = {}
+		let changed = false
+		for(let key in values){
+			let result = this._set(key, values[key])
+			if(result !== k._NO_CHANGE){
+				changed = true
+				changes[key] = result
+				this.trigger(`changed:${key}`, this, key, result)
+			}
+		}
+		if(changed){
+			this.trigger("changed", this, changes)
+		}
+		return changes
+	}
+	_set(fieldName, data){
+		if(data instanceof k.DataObject){
+			if(this.data[fieldName] instanceof k.DataObject){
+				this.data[fieldName].reset(data.data)
+			} else {
+				this.data[fieldName] = data
+			}
+		} else if(this.options.fieldDataObjects[fieldName]){
+			if(this.data[fieldName]){
+				this.data[fieldName].reset(data)
+			} else {
+				this.data[fieldName] = new this.options.fieldDataObjects[fieldName](data);					
+			}
+		} else {
+			if(this.data[fieldName] === data){
+				return k._NO_CHANGE
+			}
+			if(this.data[fieldName] instanceof k.DataObject){
+				this.data[fieldName].reset(data)
+			} else {
+				this.data[fieldName] = data
+			}
+		}
+		return this.data[fieldName]
+	}
+	reset(data={}){
+		for(var key in this.data){
+			if(typeof data[key] === "undefined"){
+				this.data[key] = null
+			}
+		}
+		this.setBatch(data)
+	}
+	equals(obj){
+		if(obj === null || typeof obj === "undefined") return false
+		if(this === obj) return true
+		if(typeof obj !== typeof this) return false
+		if(obj.get('id') === this.get('id')) return true
+		return false
+	}
+}
+
+/*
+	DataCollection represents an ordered list of k.DataModel instances
+
+	options:
+		dataObject (DataModel): the k.DataObject extending class to use to wrap each data item in this collection
+*/
+k.DataCollection = class extends k.DataObject {
+	constructor(data=[], options={}){
+		super(options)
+		this.dataObjects = []
+		for(let datum of data){
+			this.add(this.generateDataObject(datum))
+		}
+	}
+	cleanup(){
+		super.cleanup()
+		this.dataObjects.length = 0
+	}
+	at(index){
+		if(index < 0 || index > this.dataObjects.length - 1){
+			throw new Error("Index out of range: ${index}")
+		}
+		return this.dataObjects[index]
+	}
+	add(dataObject){
+		if(dataObject instanceof k.DataObject == false){
+			dataObject = this.generateDataObject(dataObject)
+		}
+		if(this.dataObjects.indexOf(dataObject) !== -1){ // TODO stop using indexOf because equality doesn't work
+			return
+		}
+		this.dataObjects.push(dataObject)
+		this.trigger("added", this, dataObject)
+	}
+	// Add an array of k.DataObjects to the end of the collection
+	addBatch(dataObjects){
+		for(let dataObject in dataObjects){
+			if(dataObject instanceof k.DataObject == false){
+				dataObject = this.generateDataObject(dataObject)
+			}
+			this.add(dataObject)
+		}
+	}
+	indexOf(dataObject){
+		for(var i=0; i < this.dataObjects.length; i++){
+			if(this.dataObjects[i].equals(dataObject)){
+				return i
+			}
+		}
+		return -1
+	}
+	remove(dataObject){
+		let index = this.indexOf(dataObject)
+		if(index === -1){
+			return
+		}
+		this.dataObjects.splice(index, 1)
+		this.trigger("removed", this, dataObject)
+	}
+	reset(data){
+		for(let obj of this.dataObjects.slice()){
+			this.remove(obj)
+		}
+		for(let datum of data){
+			this.add(this.generateDataObject(datum))
+		}
+		this.trigger("reset", this)
+	}
+	*[Symbol.iterator](){
+		for(let obj of this.dataObjects){
+			yield obj
+		}
+	}
+	get length(){
+		return this.dataObjects.length
+	}
+	generateDataObject(data){
+		let options = { collection: this }
+		if(this.options.dataObject){
+			return new this.options.dataObject(data, options)
+		}
+		return new k.DataModel(data, options)
+	}
+}
+
+/*
+	Component holds the reactive logic for a DOM element
+*/
+k.Component = k.eventMixin(class {
+	constructor(dataObject=null, options={}){
+		this.dataObject = dataObject // a k.DataModel or k.DataCollection
+		this.options = options
+		if(typeof this.options.el !== "undefined"){
+			this.el = this.options.el;
+		} else {
+			this.el = k.el.div()
+		}
+		this.boundCallbacks = [] // { callback, dataObject } to be unbound during cleanup
+		this._el.component = this
+	}
+	cleanup(){
+		super.cleanup()
+		for(let bindInfo of this.boundCallbacks){
+			bindInfo.dataObject.removeListener(bindInfo.callback)
+		}
+	}
+	// The root DOM element
+	get el(){ 
+		return this._el
+	}
+	set el(domElement){
+		if(!domElement || domElement.nodeType !== 1){
+			throw new Error(`Tried to set a non-DOM element to Component.el: ${domElement}: ${domElement.nodeType}`);
+		}
+		if(this._el){
+			delete this._el["component"]
+		}
+		this._el = domElement
+		this._el.component = this
+		this.trigger(k.Component.ElementChangedEvent, this, this._el)
+	}
+	/*
+		Set the targetElement.innerText to the value of dataObject.get(fieldName) as it changes
+		dataObject defaults to this.dataObject but can be any k.DataModel or k.DataCollection
+		formatter defaults to the identity function but can be any function that accepts the value and returns a string
+	*/
+	bindText(fieldName, targetElement, formatter=null, dataObject=this.dataObject){
+		if(formatter === null){
+			formatter = (value) => {
+				if(value === null) return ""
+				if(typeof value === "string") return value
+				return "" + value
+			}
+		}
+		let callback = () => {
+			let result = formatter(dataObject.get(fieldName))
+			targetElement.innerText = typeof result === "string" ? result : ""
+		}
+		dataObject.addListener(callback, `changed:${fieldName}`)
+		callback()
+		this.boundCallbacks.push({
+			callback: callback,
+			dataObject: dataObject
+		})
+	}
+	/*
+		Set the attributeName attribute of targetElement to the value of dataObject.get(fieldName) as it changes
+		formatter defaults to the identity function but can be any function that accepts the value and returns a string
+	*/
+	bindAttribute(fieldName, targetElement, attributeName, formatter=null, dataObject=this.dataObject){
+		if(formatter === null){
+			formatter = (value) => {
+				if(value === null) return ""
+				if(typeof value === "string") return value
+				return "" + value
+			}
+		}
+		let callback = () => {
+			targetElement.setAttribute(attributeName, formatter(dataObject.get(fieldName)));
+		}
+		dataObject.addListener(callback, `changed:${fieldName}`)
+		callback()
+		this.boundCallbacks.push({
+			callback: callback,
+			dataObject: dataObject
+		})
+	}
+})
+k.Component.ElementChangeEvent = "element-changed";
+
+/*
+	Functions that generate DOM elements like k.el.div(...) will live in k.el
+*/
+k.el = {};
+
+/*
+	domElementFunction is the behind the scenes logic for the functions like k.el.div(...)
+	Below you will find the loop that uses domElementFunction
+*/
+k.el.domElementFunction = function(tagName, ...params){
+	// Create a boring DOM element
+	let el = document.createElement(tagName)
+
+	// A convenience function to allow chaining like `let fooDiv = k.el.div().appendTo(document.body)`
+	el.appendTo = function(parent){
+		parent.appendChild(el)
+		return el
+	}
+
+	// A convenience function to allow appending strings, dictionaries of attributes, arrays of subchildren, or children
+	el.append = function(child=null){
+		if(child === null){ return }
+		if(typeof child === "string"){
+			this.appendChild(document.createTextNode(child))
+		} else if(Array.isArray(child)){
+			for(let subChild of child){
+				this.append(subChild)
+			}
+		// If it's an object but not a DOM element, consider it a dictionary of attributes
+		} else if(typeof child === "object" && typeof child.nodeType === "undefined"){
+			for(let key in child){
+				if(child.hasOwnProperty(key) == false){
+					continue
+				}
+				this.setAttribute(key, child[key])
+			}
+		} else {
+			this.appendChild(child)
+		}
+		return el
+	}
+
+	// Convenience functions to add and remove classes from this element without duplication
+	el.addClass = function(className){
+		// TODO
+	}
+	el.removeClass = function(className){
+		// TODO
+	}
+
+	// Append the children parameters
+	for(let child of params){
+		el.append(child)
+	}
+	return el
+}
+
+/* 
+	The tag names that will be used below to generate all of the element generating functions like k.el.div(...)
+	These names were ovingly copied from the excellent Laconic.js 
+	https://github.com/joestelmach/laconic/blob/master/laconic.js
+*/
+k.el.TAGS = ['a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b',
+	'base', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption',
+	'cite', 'code', 'col', 'colgroup', 'command', 'datalist', 'dd', 'del',
+	'details', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset',
+	'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5',
+	'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img',
+	'input', 'ins', 'keygen', 'kbd', 'label', 'legend', 'li', 'link', 'map',
+	'mark', 'menu', 'meta', 'meter', 'nav', 'noscript', 'object', 'ol',
+	'optgroup', 'option', 'output', 'p', 'picture', 'param', 'pre', 'progress', 
+	'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 
+	'small', 'source', 'span', 'strong', 'style', 'sub', 'summary', 'sup', 
+	'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title',
+	'tr', 'ul', 'var', 'video', 'wbr']
+
+// This loop generates the element generating functions like k.el.div(...)
+for(let tag of k.el.TAGS){
+	let innerTag = tag
+	k.el[innerTag] = function(...params){
+		return k.el.domElementFunction(innerTag, ...params)
+	}
+}
+
---
+++ test/potassium-test.js
@@ -0,0 +1,406 @@
+"use strict";
+/*
+	Tests for PotassiumES (see potassium.js)
+*/
+
+var kTest = {};
+
+// KTests to be run by k.tests.runKTests()
+kTest.K_TESTS = []
+
+/*
+	A test object that holds state. Push these into kTest.K_TESTS and use kTest.run() to run them.
+*/
+kTest.Test = class {
+	constructor(name, testFunction, setupFunction=null, teardownFunction=null){
+		this.name = typeof name === undefined ? "Unnamed test" : name
+		this.testFunction = testFunction
+		this.setupFunction = setupFunction
+		this.teardownFunction = teardownFunction
+		this.assertionCount = 0
+	}
+	test() {
+		if(typeof this.setupFunction == 'function'){
+			try {
+				this.setupFunction(this)
+			} catch (e) {
+				console.error("Setup error", e, e.stack)
+				return false
+			}
+		}
+		try {
+			this.testFunction(this)
+			return true
+		} catch (e) {
+			console.error("Test error", e, e.stack)
+			return false
+		} finally {
+			if(typeof this.teardownFunction == 'function'){
+				try {
+					this.teardownFunction(this)
+				} catch (e) {
+					console.error("Teardown error", e, e.stack)
+				}
+			}
+		}
+	}
+	assertTrue(value){
+		this.assertionCount += 1
+		if(!value){
+			throw new Error(`${value} is not true`)
+		}
+	}
+	assertInstanceOf(value, clazz){
+		this.assertionCount += 1
+		if(value instanceof clazz){
+			return
+		}
+		throw new Error(`${value} is not an instance of ${clazz}`)
+	}
+	assertNull(value){
+		this.assertionCount += 1
+		if(value !== null){
+			throw new Error(`${value} is not null`)
+		}
+	}
+	assertEqual(val1, val2){
+		this.assertionCount += 1
+		if(val1 === null){
+			if(val2 !== null){
+				throw new Error(`${val1} != ${val2}`)
+			}
+		}
+		if(val2 === null){
+			if(val1 !== null){
+				throw new Error(`${val1} != ${val2}`)
+			}
+		}
+		if(val1 == null){
+			return true
+		}
+		if(typeof val1 == "undefined"){
+			if(typeof val2 !== "undefined"){
+				throw new Error(`${val1} != ${val2}`)
+			}
+		}
+		if(typeof val2 == "undefined"){
+			if(typeof val1 !== "undefined"){
+				throw new Error(`${val1} != ${val2}`)
+			}
+		}
+		if(typeof val1 == "undefined"){
+			return true
+		}
+		if(typeof val1.equals == "function"){
+			if(val1.equals(val2) == false){
+				throw new Error(`${val1} != ${val2}`)
+			}
+		}
+		else if(val1 != val2){
+			throw new Error(`${val1} != ${val2}`)
+		}
+	}
+	assertNotEqual(val1, val2){
+		this.assertionCount += 1
+
+	}
+}
+
+/*
+	Run the tests in kTest.K_TESTS
+*/
+kTest.run = function(){
+	let passedTests = []
+	let failedTests = []
+	let assertionCount = 0
+	for(let test of kTest.K_TESTS){
+		let passed = test.test()
+		assertionCount += test.assertionCount
+		if(passed){
+			passedTests.push(test)
+		} else {
+			failedTests.push(test)
+		}
+	}
+	return [passedTests, failedTests, assertionCount]
+}
+
+/*
+	Run kTest.K_TESTS and print results to the console
+*/
+kTest.runAndLog = function(){
+	var [passedTests, failedTests, assertionCount] = kTest.run()
+	console.log(`assertions: ${assertionCount}  total: ${passedTests.length + failedTests.length}  passed: ${passedTests.length}  failed: ${failedTests.length}`)
+}
+
+/*
+	SynchronousPromise sort of acts like a Promise but is not asynchronous
+*/
+kTest.SynchronousPromise = class {
+	constructor(resolveFunction=null, rejectFunction=null){
+		this.resolveFunction = resolveFunction
+		this.rejectFunction = rejectFunction
+	}
+	then(func){
+		let result = null;
+		if(this.resolveFunction){
+			result = func(this.resolveFunction())
+		}
+		return new kTest.SynchronousPromise(()=> {
+			return result;
+		});
+	}
+	catch(func){
+		let result = null;
+		if(this.rejectFunction){
+			result = func(this.rejectFunction())
+		}
+		return new kTest.SynchronousPromise(() => {
+			return result;
+		});
+	}
+}
+
+/*
+	MockResponse sort of acts like the Response object returned from fetch
+*/
+kTest.MockResponse = class {
+	constructor(responseData={}, status=200){
+		this.responseData = responseData
+		this.status = status
+	}
+	json(){
+		return this.responseData
+	}
+}
+
+// Load this up with <url string, { data: responseData, status: INT }> for synchronousFetch to use
+kTest.SynchronousFetchMap = new Map();
+
+// This is used in setup and teardown of kTest.Tests to replace window.fetch with a synchronous version that is easier to test
+kTest.synchronousFetch = function(url){
+	return new kTest.SynchronousPromise(() => {
+		let info = kTest.SynchronousFetchMap.get(url)
+		if(!info){
+			return new kTest.MockResponse("Not found", 404)
+		}
+		return new kTest.MockResponse(info)
+	})
+}
+
+/*
+	Run the tests for Be itself, including testing k.DataModel and k.DataCollection
+*/
+kTest.testPotassium = function(){
+	kTest.K_TESTS.push(new kTest.Test("Events test", (test) => {
+		let model = new k.DataModel()
+		let receivedEvents = []
+		model.addListener((eventName, target, ...params) => { 
+			receivedEvents.push({ eventName: eventName, target: target, params: params })
+		})
+		model.addListener((eventName, target, ...params) => { 
+			receivedEvents.push({ eventName: eventName, target: target, params: params })
+		}, "changed:foo")
+		model.addListener((eventName, target, ...params) => { 
+			receivedEvents.push({ eventName: eventName, target: target, params: params })
+		}, "changed:not_foo")
+		model.set("foo", "bar")
+		test.assertEqual(receivedEvents.length, 3)
+		test.assertEqual(receivedEvents[receivedEvents.length - 2].eventName, "changed:foo")
+		test.assertEqual(receivedEvents[receivedEvents.length - 2].target, model)
+		test.assertEqual(receivedEvents[receivedEvents.length - 2].params[0], "foo")
+
+		var listener = (eventName, target, ...params) => {
+			receivedEvents.push({ eventName: eventName, target: target, params: params })
+		}
+		model.addListener(listener, "changed:baz")
+		receivedEvents.length = 0
+		model.set("baz", 23)
+		test.assertEqual(receivedEvents.length, 3)
+		model.removeListener(listener, "changed:bogus") // Wrong eventName so should not be removed
+		receivedEvents.length = 0
+		model.set("baz", 100)
+		test.assertEqual(receivedEvents.length, 3) // Should still be a listener
+		model.removeListener(listener, "changed:baz")
+		receivedEvents.length = 0
+		model.set("baz", 43)
+		test.assertEqual(receivedEvents.length, 2) // Should only match the "all" listener from above
+
+		model.cleanup()
+		receivedEvents.length = 0
+		model.trigger("changed:foo", model, "foo")
+		test.assertEqual(receivedEvents.length, 0)
+	}))
+	kTest.K_TESTS.push(new kTest.Test("Model events", (test) => {
+		class FlowersCollection extends k.DataCollection {}
+		let model = new k.DataModel(null, {
+			fieldDataObjects: { flowers: FlowersCollection }
+		})
+		let receivedEvents = []
+		model.addListener((eventName, target, ...params) => { 
+			receivedEvents.push({ eventName: eventName, target: target, params: params })
+		})
+		test.assertNull(model.get("bogus"))
+		test.assertEqual(model.get("bogus", "moon"), "moon")
+		model.set("moon", "unit")
+		test.assertEqual(model.get("moon"), "unit")
+		test.assertEqual(model.get("moon", "goon"), "unit")
+		test.assertEqual(receivedEvents.length, 2)
+		test.assertEqual(receivedEvents[0].eventName, "changed:moon")
+		test.assertEqual(receivedEvents[1].eventName, "changed")
+		model.setBatch({
+			"dink": "donk",
+			"pink": "punk"
+		})
+		test.assertEqual(model.get("dink"), "donk")
+		test.assertEqual(model.get("pink"), "punk")
+		test.assertEqual(receivedEvents.length, 5)
+		test.assertEqual(receivedEvents[2].eventName, "changed:dink")
+		test.assertEqual(receivedEvents[3].eventName, "changed:pink")
+		test.assertEqual(receivedEvents[4].eventName, "changed")
+		model.set("dink", "donk")
+		test.assertEqual(receivedEvents.length, 5); // Set to same value, should trigger no events
+
+		model.set("flowers", [{ petals: 5 }, { petals: 6 }])
+		test.assertInstanceOf(model.get("flowers"), FlowersCollection)
+		let flowerCount = 0
+		for(let flower of model.get("flowers")){
+			flowerCount++
+		}
+		test.assertEqual(flowerCount, 2)
+
+		model.set("subModel", new k.DataModel({ mesmer: "scout" }))
+		test.assertInstanceOf(model.get("subModel"), k.DataModel)
+		test.assertEqual(model.get("subModel").get("mesmer"), "scout")
+		model.set("subModel", { cheese: "tall" })
+		test.assertInstanceOf(model.get("subModel"), k.DataModel)
+		test.assertEqual(model.get("subModel").get("cheese"), "tall")
+		test.assertEqual(model.get("subModel").get("mesmer"), null)
+	}))
+	kTest.K_TESTS.push(new kTest.Test("DataCollection", (test) => {
+		let col1 = new k.DataCollection()
+		let receivedEvents = []
+		col1.addListener((eventName, target, ...params) => { 
+			receivedEvents.push({ eventName: eventName, target: target, params: params })
+		})
+		test.assertEqual(col1.length, 0)
+
+		let model1 = new k.DataModel({ foo: "bar" })
+		col1.add(model1)
+		test.assertEqual(receivedEvents.length, 1)
+		test.assertEqual(receivedEvents[0].eventName, "added")
+		test.assertEqual(col1.length, 1)
+		test.assertEqual(col1.at(0).get("foo"), "bar")
+
+		receivedEvents.length = 0
+		col1.remove(model1)
+		test.assertEqual(receivedEvents.length, 1)
+		test.assertEqual(receivedEvents[0].eventName, "removed")
+		test.assertEqual(col1.length, 0)
+		col1.remove(model1)
+		test.assertEqual(receivedEvents.length, 1)
+		test.assertEqual(col1.length, 0)
+
+		receivedEvents.length = 0
+		col1.addBatch([
+			new k.DataModel({ id: 1 }),
+			new k.DataModel({ id: 2 }),
+			new k.DataModel({ id: 3 }),
+			new k.DataModel({ id: 4 })
+		])
+		test.assertEqual(receivedEvents.length, 4)
+		for(var event in receivedEvents){
+			test.assertEqual(event.eventName, "removed")
+		}
+
+		receivedEvents.length = 0
+		col1.reset([{ id:10 }, { id:11 }])
+		test.assertEqual(receivedEvents[0].eventName, "removed")
+		test.assertEqual(receivedEvents[1].eventName, "removed")
+		test.assertEqual(receivedEvents[2].eventName, "removed")
+		test.assertEqual(receivedEvents[3].eventName, "removed")
+		test.assertEqual(receivedEvents[4].eventName, "added")
+		test.assertEqual(receivedEvents[5].eventName, "added")
+		test.assertEqual(receivedEvents[6].eventName, "reset")
+	}))
+	kTest.K_TESTS.push(new kTest.Test("SynchronousFetch", 
+		(test) => {
+			kTest.SynchronousFetchMap.set("foo", { foo:"bar"})
+			var thenCount = 0;
+			fetch("foo").then(r => r.json()).then(data => {
+				thenCount += 1;
+				test.assertEqual(data.status, 200)
+				test.assertEqual(data.foo, "bar")
+			}).catch(() => {
+				throw new Error("Should not have caught on this promise.")
+			});
+
+			fetch("bogus").then(r => r.json()).then(data => {
+				thenCount += 1;
+				test.assertEqual(data.status, 404)
+			}).catch(() => {
+				throw new Error("Should not have caught on this promise.")
+			});
+			test.assertEqual(thenCount, 2); // Make sure we reached the then functions
+		},
+		(test) => {
+			this.originalFetch = window.fetch;
+			window.fetch = kTest.synchronousFetch;
+			kTest.SynchronousFetchMap.clear();
+		},
+		(test) => {
+			window.fetch = this.originalFetch;
+		}
+	))
+	kTest.K_TESTS.push(new kTest.Test("Component configuration", (test) => {
+		let component1 = new k.Component()
+		component1.el = document.createElement("bogus")
+		test.assertEqual(component1.el.tagName.toLowerCase(), "bogus")
+
+		let fooSpan = k.el.span("Mooo", { foo:"bar" }, k.el.p({ blatz:"biz" }, "flowers")).appendTo(component1.el)
+		component1.el.appendChild(fooSpan)
+		test.assertEqual(fooSpan.getAttribute("foo"), "bar")
+		test.assertNotEqual(fooSpan.innerHTML.indexOf("blatz=\"biz\""))
+
+		component1 = k.el.div()
+		component1.append("Foo")
+		test.assertEqual(component1.innerHTML, "Foo")
+		component1.append({ bling:"ring" })
+		test.assertEqual(component1.getAttribute("bling"), "ring")
+
+		class C1 extends k.Component {
+			constructor(dataObject, options){
+				super(dataObject, options)
+				this.fooEl = k.el.div().appendTo(this.el)
+				this.bindText("foo", this.fooEl)
+				this.burfEl = k.el.div().appendTo(this.el)
+				this.bindText("burf", this.burfEl, (value) => { 
+					return (value ? value : "nothing") + " and more!"
+				})
+				this.bindAttribute("blart", this.el, "bluez")
+			}
+		}
+
+		let model1 = new k.DataModel({ foo:"bar", blart:"binz" })
+		let c2 = new C1(model1, { bibim:"bap" })
+		test.assertEqual(c2.fooEl.innerHTML, "bar")
+		test.assertEqual(c2.burfEl.innerHTML, "nothing and more!")
+		test.assertEqual(c2.el.getAttribute("bluez"), "binz")
+
+		model1.set("foo", "biz")
+		test.assertEqual(c2.fooEl.innerHTML, "biz")
+		model1.set("foo", null)
+		test.assertEqual(c2.fooEl.innerHTML, "")
+		model1.set("foo", 23)
+		test.assertEqual(c2.fooEl.innerHTML, "23")
+
+		model1.set("burf", "sugar")
+		test.assertEqual(c2.burfEl.innerHTML, "sugar and more!")
+
+		model1.set("blart", "floop")
+		test.assertEqual(c2.el.getAttribute("bluez"), "floop")
+	}))
+
+	kTest.runAndLog()
+}
+
